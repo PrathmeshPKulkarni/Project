5a848ea2ec00e26761a9958afec487e45277c3b6
Prathmesh Prakash Kulkarni <75133844+PrathmeshPKulkarni@users.noreply.github.com>
2022-04-07 15:44:18 +0530

    Add files via upload

--- practo.ma/src/test/java/practo/ma/NewTest.java
+++ practo.ma/src/test/java/practo/ma/NewTest.java
@@ -1,6 +1,9 @@
 package practo.ma;
 
 import org.testng.annotations.Test;
+
+
+
 //import automation.WebElement;
 import org.testng.annotations.BeforeTest;
 
@@ -16,97 +19,59 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
+
 public class NewTest {
-	
+	public static WebDriver driver;
+	public static Properties prop;
+	public static String sAppURL;
+	public static String uname;
+	public static String password;
   @Test
-  public void f() {
-	  
+  public void f() throws InterruptedException, IOException {
+	  System.out.println("Entered Test Method");
+	  Thread.sleep(3000);
+	  String[] data = ExcelData.readXL1();
+		System.out.println(data[1]);
+		driver.findElement(By.xpath("//*[@id=\\\"c-omni-container\\\"]/div/div[1]/div[1]/input")).sendKeys(data[1]);
+		 WebElement sea = driver.findElement(By.xpath ("//*[@id=\'c-omni-container\']/div/div[2]/div[1]/input"));//*[@id="c-omni-container"]/div/div[2]/div[1]/input
+		 sea.sendKeys("Hospital");
+	 /* WebElement search = driver.findElement(By.xpath ("//*[@id=\"c-omni-container\"]/div/div[1]/div[1]/input"));//
+	  System.out.println(search.toString());
+		 search.clear();
+		 
+		 search.sendKeys("Pune");
+		 WebElement sea = driver.findElement(By.xpath ("//*[@id=\'c-omni-container\']/div/div[2]/div[1]/input"));//*[@id="c-omni-container"]/div/div[2]/div[1]/input
+		 sea.sendKeys("Hospital");
+		 sea.sendKeys(Keys.RETURN);
+		 Thread.sleep(3000);
+		 // For Accredited Button
+		 driver.findElement(By.id("Accredited_checkbox")).click(); 
+		 //for 24*7 button
+		 driver.findElement(By.id("Open_24X7_checkbox")).click();*/
 	
   }
   @BeforeTest
   public void beforeTest()throws FileNotFoundException, IOException {
 	  System.setProperty("webdriver.chrome.driver","E:\\Capita\\chromedriver_win32\\chromedriver.exe");
-		 WebDriver driver = new ChromeDriver();
-	  WebElement search = driver.findElement(By.xpath ("//*[@id=\"c-omni-container\"]/div/div[1]/div[1]/input"));
-		 search.clear();
-		 search.sendKeys("Pune");
-		 WebElement sea = driver.findElement(By.xpath ("//*[@id=\"c-omni-container\"]/div/div[2]/div[1]/input"));//*[@id="c-omni-container"]/div/div[2]/div[1]/input
-		 sea.sendKeys("Hospital");
-		 sea.sendKeys(Keys.RETURN);// //*[@id="container"]/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/span/span
-		 driver.findElement(By.xpath("//*[@id=\"container\"]/div[3]/div/div[1]/div/div/header/div[1]/div/div[2]/label/span/span")).click();
-	  
-	  WebDriver driver1 = null;
-		Properties prop = null;
-		String sAppURL=null;
-		String username=null;
-		String password=null;
-		
-		//This function is used to read data from specified cell of Excel sheet once you give Excelsheet name and path
-		
-				/*public static String HA_GF_readXL (int row, String column, String strFilePath) throws IOException 
-				{
-					
-					File file =    new File("E:\Capita\Excelfile");
-					FileInputStream inputStream = new FileInputStream(file);
-					HSSFWorkbook wb=new HSSFWorkbook(inputStream);
-					HSSFSheet sheet=wb.getSheet("location");
-					HSSFRow row1=sheet.getRow(1);
-							
-					int rowCount=sheet.getLastRowNum()-sheet.getFirstRowNum();
-			        System.out.println(rowCount);
-			        //iterate over all the row to print the data present in each cell.
-			        for(int i=0;i<=rowCount;i++){
-			            
-			            //get cell count in a row
-			            int cellcount=sheet.getRow(i).getLastCellNum();
-			            System.out.println(cellcount);
-			            //iterate over each cell to print its value
-			            System.out.println("Row"+ i+" data is :");
-			            
-			            for(int j=0;j<cellcount;j++){
-			                System.out.print(sheet.getRow(i).getCell(j).getStringCellValue() +",");
-			            }
-			            System.out.println();
-			        }
-			    
-				//System.out.println("NO MATCH FOUND IN GIVEN FILE: PROBLEM IS COMING FROM DATA FILE");
-				
-				return null;
-				}*/
-		// TODO Auto-generated method stub
-		try {
-			prop = new Properties();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		  
-		  prop.load(new FileInputStream("C:\\Users\\PRATHAMESH\\eclipse-workspace\\practo.ma\\Configuration"));
-		  sAppURL = prop.getProperty("sAppURL");
-		  username = prop.getProperty("uname");
-		  password = prop.getProperty("password");
-		  System.out.println(sAppURL);
-		  System.out.println(username);
-		  System.out.println(password);
-		//System.setProperty("webdriver.chrome.driver\",\"E:\\\\Capita\\\\chromedriver_win32\\\\chromedriver.exe");
-		//driver = new ChromeDriver();
-		driver1.get(sAppURL);
-		driver1.manage().window().maximize();
-		driver1.findElement(By.id("username")).sendKeys(username);
-		driver1.findElement(By.id("password")).sendKeys(password);
-		driver1.findElement(By.id("login")).click(); 
-	  
-	  
-	 /* System.setProperty("webdriver.chrome.driver","E:\\Capita\\chromedriver_win32\\chromedriver.exe");
-		 WebDriver driver = new ChromeDriver();
-		 driver.get("https://accounts.practo.com/login?next=%2Fcheckid_request&intent=fabric");
-		 //driver.findElement(By.className("nav-login-mobile btn-border nav-interact")).click();//nav-login-mobile btn-border nav-interact
-
-		 WebElement uname = driver.findElement(By.id ("username"));// username
-		 uname.sendKeys("7720936922"); 
-		 driver.findElement(By.id("password")).sendKeys("Pratik@7720");// password
-		 driver.findElement(By.id("login")).click();*/
-	  
-	  
+			driver = new ChromeDriver();
+		 try {
+				prop = new Properties();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			  
+			  prop.load(new FileInputStream("C:\\Users\\PRATHAMESH\\eclipse-workspace\\practo.ma\\Configuration\\User_Details.properties"));
+			  sAppURL = prop.getProperty("sAppURL");
+			  uname = prop.getProperty("uname");
+			  password = prop.getProperty("password");
+			  System.out.println(sAppURL);
+			  System.out.println(uname);
+			  System.out.println(password);
+			driver.get(sAppURL);
+			driver.manage().window().maximize();
+			driver.findElement(By.id("username")).sendKeys(uname);
+			driver.findElement(By.id("password")).sendKeys(password);
+			driver.findElement(By.id("login")).click();	  
   }
 
   @AfterTest
